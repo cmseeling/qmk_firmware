ac2755d5eeeaeffd9a0624328cc294734f92a729
cmseeling <cmseeling@gmail.com>
2019-11-18 19:41:37 -0600

    added commented out mouse settings for future tweaking

--- keyboards/keebio/iris/keymaps/cmseeling/config.h
+++ keyboards/keebio/iris/keymaps/cmseeling/config.h
@@ -29,3 +29,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
+
+// #define MOUSEKEY_DELAY             300
+// #define MOUSEKEY_INTERVAL          50
+// #define MOUSEKEY_MAX_SPEED         10
+// #define MOUSEKEY_TIME_TO_MAX       20
+// #define MOUSEKEY_WHEEL_MAX_SPEED   8
+// #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
