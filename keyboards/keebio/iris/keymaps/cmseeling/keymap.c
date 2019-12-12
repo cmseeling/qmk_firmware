66554d99afe7042ac81b71d5634bf4427e73038d
cmseeling <cmseeling@gmail.com>
2019-12-11 19:14:58 -0600

    home/end and home select/end select to symbol layer

--- keyboards/keebio/iris/keymaps/cmseeling/keymap.c
+++ keyboards/keebio/iris/keymaps/cmseeling/keymap.c
@@ -31,7 +31,9 @@ enum {
   TD_ALT,
   TD_SLCK,
   TD_ESC,
-  TD_ENC
+  TD_ENC,
+  TD_HSL,
+  TD_ESL
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //----------+----------+----------+----------+----------+----------+                            ----------+----------+----------+----------+----------+----------+
       KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,                                  KC_F7,     KC_F8,     KC_F9,    KC_F10,    KC_F11,    KC_F12,
   //----------+----------+----------+----------+----------+----------+                            ----------+----------+----------+----------+----------+----------+
-     _______,   _______,   _______,   _______,   _______,   _______,                               KC_UNDS,   KC_SLSH,   KC_LBRC,   KC_RBRC,   KC_BSLS,   _______,
+     _______,   _______,   _______,   _______,  TD(TD_HSL), KC_HOME,                               KC_UNDS,   KC_SLSH,   KC_LBRC,   KC_RBRC,   KC_BSLS,   _______,
   //----------+----------+----------+----------+----------+----------+                            ----------+----------+----------+----------+----------+----------+
-     RGB_HUI,   RGB_SAI,   RGB_VAI,   _______,   _______,   _______,                                KC_EXLM,  KC_EQL,    KC_AMPR,   KC_PIPE,   _______,   _______,
+     RGB_HUI,   RGB_SAI,   RGB_VAI,   _______,  TD(TD_ESL),  KC_END,                                KC_EXLM,  KC_EQL,    KC_AMPR,   KC_PIPE,   _______,   _______,
   //----------+----------+----------+----------+----------+----------+----------+      ----------+----------+----------+----------+----------+----------+----------+
      RGB_HUD,   RGB_SAD,   RGB_VAD,   _______,   _______,   _______,   _______,         _______,   KC_PLUS,   KC_MINS,   _______,   _______,   _______,   _______,
   //----------+----------+----------+----------+----------+----------+----------+      ----------+----------+----------+----------+----------+----------+----------+
@@ -68,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //----------+----------+----------+----------+----------+----------+                            ----------+----------+----------+----------+----------+----------+
      KC_PSCR,   _______,   _______,    KC_F4,      KC_F5,     KC_F6,                               _______,     KC_7,      KC_8,      KC_9,    KC_SLSH,   KC_EQL,
   //----------+----------+----------+----------+----------+----------+                            ----------+----------+----------+----------+----------+----------+
-     _______,   _______,    KC_UP,    _______,   KC_PGUP,   KC_HOME,                               _______,     KC_4,      KC_5,      KC_6,    KC_ASTR,   _______,
+     _______,   _______,    KC_UP,    _______,   KC_PGUP,    KC_HOME,                              _______,     KC_4,      KC_5,      KC_6,    KC_ASTR,   _______,
   //----------+----------+----------+----------+----------+----------+                            ----------+----------+----------+----------+----------+----------+
-     _______,   KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_PGDN,   KC_END,                                _______,     KC_1,      KC_2,      KC_3,    KC_PLUS,   _______,
+     _______,   KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_PGDN,    KC_END,                               _______,     KC_1,      KC_2,      KC_3,    KC_PLUS,   _______,
   //----------+----------+----------+----------+----------+----------+----------+      ----------+----------+----------+----------+----------+----------+----------+
      _______,   _______,   _______,   _______,   _______,   _______,   _______,         _______,   _______,     KC_0,     KC_DOT,   _______,   KC_MINS,   _______,
   //----------+----------+----------+----------+----------+----------+----------+      ----------+----------+----------+----------+----------+----------+----------+
@@ -250,6 +252,18 @@ void enc_dance_reset (qk_tap_dance_state_t *state, void *user_data) {
   ql_tap_state.state = 0;
 }
 
+void home_select_finish (qk_tap_dance_state_t *state, void *user_data) {
+    register_code(KC_LSFT);
+    tap_code(KC_HOME);
+    unregister_code (KC_LSFT);
+}
+
+void end_select_finish (qk_tap_dance_state_t *state, void *user_data) {
+    register_code(KC_LSFT);
+    tap_code(KC_END);
+    unregister_code (KC_LSFT);
+}
+
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_GRV]  = ACTION_TAP_DANCE_FN(grv_tap_finish),
   [TD_CTRL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctrl_dance_finish, ctrl_dance_reset),
@@ -257,6 +271,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_SLCK] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
   [TD_ESC]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, esc_tap, esc_dance_reset),
   [TD_ENC]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, enc_tap_finish, enc_dance_reset),
+  [TD_HSL]  = ACTION_TAP_DANCE_FN(home_select_finish),
+  [TD_ESL]  = ACTION_TAP_DANCE_FN(end_select_finish),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
